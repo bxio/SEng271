c1b62c08f72ccdda72b16ee595c5ac6af4e0c3db
Bill Xiong <billxiong99@gmail.com>
2013-06-27 18:30:55 -0700

    Added JavaDocs for Dice.

--- Dice.java
+++ Dice.java
@@ -1,5 +1,4 @@
-/**
- *
+/**Dice Used with Ludo game. This one isn't rigged, I promise.
  * @author bill
  */
 import java.util.Random;
@@ -13,12 +12,20 @@ public class Dice {
         generator = new Random();
         log = new ArrayList<Integer>();
     }
-
+	/** Rolls the dice and returns the result. 
+	 * Also adds the result to the dice log.
+	 * 
+	 * @return			An integer between 1 and 6 (as the result of the dice roll.)
+	 */
     public int roll(){
         this.result = generator.nextInt(5)+1;
         log.add(new Integer(result));
         return this.result;
     }
+	/** returns a log of all of the dice results.
+	 * 
+	 * @return			ArrayList<Integer> of all of the results of the die
+	 */
     public ArrayList<Integer> getResults(){
         return this.log;
     }
