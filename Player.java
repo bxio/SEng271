9f66ffcf508d9b3e0568ee0f1109d1b64e0256e4
Bill Xiong <billxiong99@gmail.com>
2013-06-29 09:54:05 -0700

    More Javadocs

--- Player.java
+++ Player.java
@@ -67,23 +67,41 @@ public class Player {
 	public String getColour(){
 		return this.colour;
 	}
-	
+	/** Sets the position of the player
+	* 
+	* @param position the integer of the player's starting position. If the integer is not
+	* 1, 11, 21,or 31 the method will throw an exception.
+	*/
 	public void setStartingPosition(int position){
-		if(position % 10 == 1){
+		if((position % 10 == 1) && position <=31){
 			this.start = position;
 		}else{
 			throw new UnsupportedOperationException("Invalid startiong position Given!");
 		}
 	}
+	/** Returns the starting position of the player
+	* 
+	* @return integer of the starting position of the player
+	*/
 	public int getStartingPosition(){
 		return this.start;
 	}
 	/** Sets the Player's Strategy
 	 * 
-	 * @param option	Number of the strategy (from 1 to 6)
+	 * @param option	Number of the strategy (from 0 to 6)
+	 * 
+	 *	0: Human Strategy
+	 *	1: Aggressive Strategy
+	 *	2: Cautious Strategy
+	 *	3: Defensive Strategy
+	 *	4: Move First Strategy
+	 *	5: Move Last Strategy
+	 *	6: Evil Strategy
 	 */
 	public void setStrategy(int option){
-		if(option == 1){
+		if(option == 0){
+			this.strategy = new HumanStrategy();
+		}else if(option == 1){
 			this.strategy = new AggressiveStrategy();
 		}else if(option == 2){
 			this.strategy = new CautiousStrategy();
@@ -93,18 +111,30 @@ public class Player {
 			this.strategy = new MoveFirstStrategy();
 		}else if(option == 5){
 			this.strategy = new MoveLastStrategy();
+		}else if(option == 6){
+			this.strategy = new BrutalStrategy();
 		}else{
 			throw new UnsupportedOperationException("Invalid Strategy option Given!");
 		}
 	}
-	public String getStrategy(){
+	/** Returns the player's strategy
+	* 
+	* @return the player's strategy
+	*/
+	public Strategy getStrategy(){
+		return this.strategy;
+	}
+	/** Returns the name of the player's strategy
+	* 
+	* @return the name of the strategy
+	*/
+	public String getStrategyName(){
 		return this.strategy.getName();
 	}
+	/** Does the move recommended by the strategy.
+	* 
+	*/
 	public void execute(){
 		
-	}
-	
-	public Player passTurn(){
-		return null;
 	}
 }
