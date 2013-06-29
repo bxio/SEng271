f9c31cfdf3a8f3a5c1ff50452994def0cc4b590b
Bill Xiong <billxiong99@gmail.com>
2013-06-29 11:48:43 -0700

    Added getLastResult() method to dice.

--- Dice.java
+++ Dice.java
@@ -15,16 +15,23 @@ public class Dice {
 	/** Rolls the dice and returns the result. 
 	 * Also adds the result to the dice log.
 	 * 
-	 * @return			An integer between 1 and 6 (as the result of the dice roll.)
+	 * @return	An integer between 1 and 6 (as the result of the dice roll.)
 	 */
     public int roll(){
         this.result = generator.nextInt(5)+1;
         log.add(new Integer(result));
         return this.result;
     }
+	/** returns the last dice roll.
+	 * 
+	 * @return	an integer representing the result of the roll
+	 */
+    public int getLastResult(){
+        return this.log.get(-1);
+    }
 	/** returns a log of all of the dice results.
 	 * 
-	 * @return			ArrayList<Integer> of all of the results of the die
+	 * @return	ArrayList<Integer> of all of the results of the die
 	 */
     public ArrayList<Integer> getResults(){
         return this.log;
