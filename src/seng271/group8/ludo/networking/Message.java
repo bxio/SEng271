2568bd6fe2c544fe6c68898c00a08a220260cd50
afehr <alastair.fehr@gmail.com>
2013-07-08 22:42:11 -0700

    Some networking stuff, nothing works yet

---
+++ src/seng271/group8/ludo/networking/Message.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seng271.group8.ludo.networking;
+
+/**
+ *
+ * @author Alastairs
+ */
+public class Message {
+    
+    public Message() {
+        
+    }
+    
+}
---
+++ src/seng271/group8/ludo/networking/Receive.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seng271.group8.ludo.networking;
+
+import java.net.Socket;
+
+/**
+ *
+ * @author Alastairs
+ */
+public class Receive implements Runnable {
+
+    public Receive(Socket s) {
+        
+    }
+    
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
