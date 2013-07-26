674033196d3bf139ff3407c8787f0bd7693ebe20
Bill Xiong <billxiong99@gmail.com>
2013-07-26 13:26:21 -0700

    Added comments for Move.java

--- src/seng271/group8/ludo/model/Move.java
+++ src/seng271/group8/ludo/model/Move.java
@@ -11,12 +11,12 @@ import java.util.LinkedList;
  * @author Alastairs
  */
 public class Move {
-    private Pawn pawn;
-    private int roll;
-    private LinkedList<Square> squares;
-    private Boolean human = false;
-    private Boolean kick = false;
-    private Move kickMove;
+    private Pawn pawn;///< the pawn to be moved
+    private int roll;///< the number it 
+    private LinkedList<Square> squares;///< the squares that the pawn pass through. We need this in order to animate the moves on the board.
+    private Boolean human = false;///< used in RollHandler to make the human strategy asychronous
+    private Boolean kick = false;///< whether the move will kick another pawn
+    private Move kickMove;///< the move of the pawn after it's kicked
     
     public Move() {
         
@@ -44,27 +44,45 @@ public class Move {
     public Move getKickMove() {
         return this.kickMove;
     }
-    
+    /** 
+	 * 
+	 * @return whether this pawn belongs to a human
+	 */
     public Boolean isHuman() {
         return human;
     }
-    
+    /** 
+	 * 
+	 * @return the pawn to be moved
+	 */
     public Pawn getPawn() {
         return this.pawn;
     }
-    
+    /** 
+	 * 
+	 * @return whether this pawn belongs to a human
+	 */
     public int getRoll() {
         return this.roll;
     }
-    
+    /** 
+	 * 
+	 * @return whether performing this move will kick another pawn
+	 */
     public Boolean doesKick() {
         return this.kick;
     }
-    
+    /** 
+	 * 
+	 * @return the pawn that is about to be kicked
+	 */
     public Pawn getKicked() {
         return squares.getLast().getPawn();
     }
-    
+    /** 
+	 * 
+	 * @return the squares to be animated
+	 */
     public LinkedList<Square> getSquares() {
         return this.squares;
     }
