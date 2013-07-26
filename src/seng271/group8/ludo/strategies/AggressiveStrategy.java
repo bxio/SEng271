1013402e5687c21540d89e619b781e03e91c9eae
Bill Xiong <billxiong99@gmail.com>
2013-07-26 13:27:36 -0700

    Aggressive and Cautious coded. Both require extensive testing.

--- src/seng271/group8/ludo/strategies/AggressiveStrategy.java
+++ src/seng271/group8/ludo/strategies/AggressiveStrategy.java
@@ -1,6 +1,8 @@
 package seng271.group8.ludo.strategies;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import seng271.group8.ludo.model.Move;
 
 /**
@@ -15,9 +17,33 @@ public class AggressiveStrategy extends AbstractStrategy {
         }
 
 	public Move getMove(List<Move> moves) {
+			List<Move> kickmoves = new ArrayList<Move>(), nonkickmoves = new ArrayList<Move>();
+			Random rng = new Random();
             Move bestMove = null;
-            if(!moves.isEmpty())
-                bestMove =  moves.get(0);
-            return bestMove;
+
+			//loop through the moves list, find the ones that result in other pawns being kicked.
+			if(moves.isEmpty()){
+				return null;
+			}else if(moves.size() == 1){
+				return moves.get(0);
+			}else{
+				for(int i=0;i<moves.size();i++){
+					if(moves.get(i).doesKick()){
+						kickmoves.add(moves.get(i));
+					}else{
+						nonkickmoves.add(moves.get(i));
+					}
+				}
+				if(!kickmoves.isEmpty()){
+					//there is a move that kicks a pawn. Randomly pick a victim
+					bestMove = kickmoves.get(rng.nextInt(kickmoves.size()));
+				}else{
+					//there is no move that kicks another pawn.
+					bestMove = nonkickmoves.get(rng.nextInt(nonkickmoves.size()));
+				}
+				return bestMove;
+			}
+			
+			
 	}
 }
--- src/seng271/group8/ludo/strategies/CautiousStrategy.java
+++ src/seng271/group8/ludo/strategies/CautiousStrategy.java
@@ -1,6 +1,8 @@
 package seng271.group8.ludo.strategies;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import seng271.group8.ludo.model.Move;
 
 /**
@@ -15,6 +17,31 @@ public class CautiousStrategy extends AbstractStrategy {
         }
 
 	public Move getMove(List<Move> moves) {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		List<Move> nonkickmoves = new ArrayList<Move>(), kickmoves = new ArrayList<Move>();
+		Random rng = new Random();
+		Move bestMove = null;
+
+		//loop through the moves list, find the ones that result in other pawns being kicked.
+		if(moves.isEmpty()){
+			return null;
+		}else if(moves.size() == 1){
+			return moves.get(0);
+		}else{
+			for(int i=0;i<moves.size();i++){
+				if(moves.get(i).doesKick()){
+					kickmoves.add(moves.get(i));
+				}else{
+					nonkickmoves.add(moves.get(i));
+				}
+			}
+			if(kickmoves.isEmpty()){
+				//there are no moves that kick.
+				bestMove = moves.get(rng.nextInt(nonkickmoves.size()));
+			}else{
+				bestMove = moves.get(rng.nextInt(kickmoves.size()));
+			}
+			return bestMove;
+		}
+
 	}
 }
