0f35e3022cda7f525e6db0071b99efd9c00bef31
hrky <hrkynn@gmail.com>
2013-07-30 01:31:19 -0700

    Game state panel is divided into three components
    Three panels are added onto the game state panel.

--- src/seng271/group8/ludo/ui/GameStatePanel.java
+++ src/seng271/group8/ludo/ui/GameStatePanel.java
@@ -4,12 +4,16 @@
  */
 package seng271.group8.ludo.ui;
 
+import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import seng271.group8.ludo.GameLogic;
 
 import seng271.group8.ludo.actions.DiceRollAction;
@@ -36,48 +40,65 @@ public class GameStatePanel extends JPanel{
     }
     
     public void setup() {
-    	
-    	this.setLayout(new GridBagLayout());
-    	GridBagConstraints gbc = new GridBagConstraints();
-    	Dimension bSmallSize = new Dimension(80, 30);
-    	
-    	// Locate "Roll" button onto the center
+        
+        this.setLayout(new BorderLayout());
+        JPanel contentPaneCenter = new JPanel();
+        JPanel contentPaneLeft = new JPanel();
+        JPanel contentPaneRight = new JPanel();
+        
+        contentPaneCenter.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+    	Dimension buttonSize = new Dimension(80, 30);
+        
+        // Locate "Roll" button onto the center
     	gbc.gridx = 1;
         gbc.gridy = 1;
         
         diceRoll = new JButton("Roll");
         diceRoll.setPreferredSize(new Dimension(80, 60));
         diceRoll.addActionListener(new DiceRollAction(game));
-        this.add(diceRoll);
-        this.add(diceRoll, gbc);
-        
+        contentPaneCenter.add(diceRoll);
+        contentPaneCenter.add(diceRoll, gbc);
+
         // Additional buttons
         gbc.gridx = 0;
         gbc.gridy = 0;     
         newGame = new JButton("New");
-        newGame.setPreferredSize(bSmallSize);
-        this.add(newGame, gbc);
+        newGame.setPreferredSize(buttonSize);
+        contentPaneCenter.add(newGame, gbc);
         
         gbc.gridx = 2;
         gbc.gridy = 0;      
         restart = new JButton("Restart");
-        restart.setPreferredSize(bSmallSize);
-        this.add(restart, gbc);
+        restart.setPreferredSize(buttonSize);
+        contentPaneCenter.add(restart, gbc);
         restart.addActionListener(new RestartGameAction());
         
         gbc.gridx = 0;
         gbc.gridy = 2;    
         options = new JButton("Options");
-        options.setPreferredSize(bSmallSize);
-        this.add(options, gbc);
+        options.setPreferredSize(buttonSize);
+        contentPaneCenter.add(options, gbc);
         options.addActionListener(new OptionsAction());
         
         gbc.gridx = 2;
         gbc.gridy = 2;   
         quit = new JButton("Quit");
-        quit.setPreferredSize(bSmallSize);
-        this.add(quit, gbc);
+        quit.setPreferredSize(buttonSize);
+        contentPaneCenter.add(quit, gbc);
         quit.addActionListener(new QuitGameAction());
+        
+        // Set titles for player's turn status and roll history
+        JLabel players = new JLabel("Players");
+        JLabel rollHist = new JLabel("Roll History");
+        
+        contentPaneLeft.add(players);
+        contentPaneRight.add(rollHist);
+        
+        // Add three main components of game state panel
+        this.add(contentPaneCenter, BorderLayout.CENTER);
+        this.add(contentPaneLeft, BorderLayout.WEST);
+        this.add(contentPaneRight, BorderLayout.EAST);
 
     }
 
