a176947def9c616a9c05318ae3fd80c0f879bf99
Bill Xiong <billxiong99@gmail.com>
2013-07-30 14:32:43 -0700

    MoveLast Strategy tested working.

--- src/seng271/group8/ludo/strategies/MoveLastStrategy.java
+++ src/seng271/group8/ludo/strategies/MoveLastStrategy.java
@@ -1,7 +1,10 @@
 package seng271.group8.ludo.strategies;
 
+import java.util.ArrayList;
 import java.util.List;
 import seng271.group8.ludo.model.Move;
+import seng271.group8.ludo.model.PathSegment;
+import seng271.group8.ludo.model.Pawn;
 
 /** Give preference to moving the hindmost pawn.
  *
@@ -14,7 +17,42 @@ public class MoveLastStrategy extends AbstractStrategy {
 
 	public Move getMove(List<Move> moves) {
 		//iterate through the board and move my pawn that is furthest behind
-
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		//TODO: Bill prioritize pawns leaving home.
+		if(moves.isEmpty()){
+			return null;
+		}else if(moves.size() == 1){
+			return moves.get(0);
+		}else{
+			List<Pawn> placement = new ArrayList<Pawn>();
+			
+			PathSegment path = moves.get(0).getPawn().getOwner().getPath().getFirst();
+			while(path != null){
+				//if pawn is not null and not in goal, add to list
+				if(path.getSquare().getPawn() != null && !path.getSquare().getPawn().isOnGoal()){
+					placement.add(path.getSquare().getPawn());
+				}
+				path = path.getNext();
+			}
+			
+			//check if home is empty. If it is, and roll is 6, prioritize pawns leaving home.			
+			if(moves.get(0).getRoll() != 6 || moves.get(0).getPawn().getOwner().getPath().getFirst().getSquare().getPawn() != null){
+				//I didn't roll 6 or my home is full.
+				for(int i=0;i<moves.size();i++){
+					if(placement.get(0) == moves.get(i).getPawn()){
+						return moves.get(i);
+					}
+				}
+			}else{
+				//I rolled a 6 and my home is empty
+				//return the first move that moves a pawn NOT on the list
+				System.out.println("6 was rolled and home empty.");
+				for(int i=0;i<moves.size();i++){
+					if(!placement.contains(moves.get(i).getPawn())){
+						return moves.get(i);
+					}
+				}
+			}
+			return moves.get(0);
+		}
 	}
 }
